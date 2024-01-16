08afeb543ea0d9e8f7361ce7a1ae9ddc1603700b
Nagihan14C <nagihan_akalin@hotmail.com>
2024-01-16 13:06:17 +0100

    Finjusteringar

--- CarProject.Data/Contexts/CarShopContexts.cs
+++ CarProject.Data/Contexts/CarShopContexts.cs
@@ -5,31 +5,21 @@ using Microsoft.EntityFrameworkCore;
 namespace CarProject.Data.Contexts;
 
 public class CarShopContext(DbContextOptions<CarShopContext> options) : DbContext(options)
-
 {
-
     public DbSet<Brand> Brands => Set<Brand>();
-
     public DbSet<Car> Cars => Set<Car>();
-
     public DbSet<Color> Colors => Set<Color>();
-
     public DbSet<Filter> Filters => Set<Filter>();
-
     public DbSet<CategoryFilter> CategoryFilters => Set<CategoryFilter>();
-
     public DbSet<VehicleType> VehicleTypes => Set<VehicleType>();
-
     public DbSet<Category> Categories => Set<Category>();
 
     protected override void OnModelCreating(ModelBuilder builder)
-
     {
 
         base.OnModelCreating(builder);
 
         // Define composite keys for join tables
-
         
 
         builder.Entity<CarColor>()
--- CarProject.Data/Entites/CarColor.cs
+++ CarProject.Data/Entites/CarColor.cs
@@ -4,11 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CarProject.Data.Entites
+namespace CarProject.Data.Entites;
+
+public class CarColor
 {
-    public class CarColor
-    {
-        public int CarId { get; set; }
-        public int ColorId { get; set; }
-    }
+    public int CarId { get; set; }
+    public int ColorId { get; set; }
 }
