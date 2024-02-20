93b69b09b34231f052f61d3e874fc5b262dfb80d
Emiilijaaa <emmafilip17@gmail.com>
2024-02-20 14:04:47 +0100

    nugget update

--- CarShop.API.DTO/DTOs/Usings.cs
+++ CarShop.API.DTO/DTOs/Usings.cs
@@ -1,7 +1,6 @@
 ﻿global using CarProject.Data.Shared.Interfaces;
 global using CarProject.Data.Shared.Enums;
 global using CarShop.API.DTO.DTOs;
-global using Microsoft.EntityFrameworkCore.Diagnostics;
 global using CarProject.API.DTO;
 global using CarShop.API.DTO;
 
